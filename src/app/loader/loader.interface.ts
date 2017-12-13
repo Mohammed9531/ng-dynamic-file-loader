4128e8afc967abc7990e0f27009c3ffcf6066551
Mohammed9531 <mohd.shoukath7866@gmail.com>
2017-12-13 00:39:37 -0600

    chore(loader): added comments.

--- src/app/loader/loader.interface.ts
+++ src/app/loader/loader.interface.ts
@@ -2,7 +2,11 @@ import { AsyncSubject } from "rxjs/Rx";
 
 /**
  * @author: Shoukath Mohammed 
- */
+ **
+/**
+ ** @name: LoaderOptions
+  * @type: Interface
+  */
 export interface LoaderOptions {
   url: string;
   async?: boolean;
@@ -13,17 +17,29 @@ export interface LoaderOptions {
   loadAfterElement?: HTMLElement;
 }
 
+/**
+ * @name: NodeLoadEvent<T>
+ * @type: Interface
+ */
 export interface NodeLoadEvent<T> {
   el?: T;
   options?: LoaderOptions;
   isLoaded$?: AsyncSubject<boolean>;
 }
 
+/**
+ * @name: NodeOptions
+ * @type: Interface
+ */
 export interface NodeOptions {
   options?: LoaderOptions;
   isLoaded$: AsyncSubject<boolean>;
 }
 
+/**
+ * @name: NodePreset
+ * @type: Interface
+ */
 export interface NodePreset {
   originalValue: any;
   fallbackValue: any;
